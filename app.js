7700842012073a18877721c214b912db80858d76
IbanLafitte <ibou64100@icloud.com>
2024-11-06 13:22:51 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+//Exercice 1
+
+console.log("Javascript c'est super")
+
+//Exercice 2
+
+console.log("Javascript c'est super")
+
+//Exercice 3
+
+console.log("Afficher un \\ n'est pas si évident.")
+
+//Exercice 4
+
+console.log(`Ceci est la première ligne Ceci est la deuxième ligne`)
+
+//Exercice 5
+
+var nom = "Bob";
+
+console.log('Bonjour ' + nom + ', aujourd\'hui nous faisons une promo "la fidélité paye" !')
